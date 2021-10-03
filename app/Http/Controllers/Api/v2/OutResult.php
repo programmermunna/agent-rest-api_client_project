1e830eec6d40b13f356d5ce287203c3f53d8d60e
hanancikatech <hanan.cikaloka@gmail.com>
2021-10-03 18:50:49 +0700

    PANAGDEV-457-PANAGDEV-465| Adding Seeder and Get Result number is to latest

--- app/Http/Controllers/Api/v2/OutResult.php
+++ app/Http/Controllers/Api/v2/OutResult.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v2;
 
 use App\Http\Controllers\ApiController;
+use App\Traits\CustomPaginate;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 
@@ -11,11 +12,13 @@ use Illuminate\Support\Facades\DB;
  */
 class OutResult extends ApiController
 {
+	use CustomPaginate;
+
 	/**
 	 * Get all Togel Result Number 
 	 * @return JsonResponse 
 	 */
-	public function getAllResult() : JsonResponse
+	public function getAllResult() 
 	{
 		$result = DB::table('togel_results_number')
 			->join("constant_provider_togel", 'togel_results_number.constant_provider_togel_id', '=', 'constant_provider_togel.id')
@@ -31,12 +34,10 @@ class OutResult extends ApiController
 				'togel_results_number.number_result_4',
 				'togel_results_number.number_result_5',
 				'togel_results_number.number_result_6',
-			])->get();
+			])
+				->latest('togel_results_number.created_at')
+				->get();
 
-		return response()->json([
-			'code' => 200,
-			'status' => 'success',
-			'data' => $result
-		]);
+		return $this->paginate($result , 25);
 	}	
 }
