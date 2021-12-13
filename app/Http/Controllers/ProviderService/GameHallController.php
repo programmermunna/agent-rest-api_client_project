41801f36b487c58df84349b2ccebf0885cdffae0
yoga-saputra1 <yogasaputra.cikatech@gmail.com>
2021-12-13 14:23:02 +0700

    bug/PANAGDEV-1584/PANAGDEV-1638/BUG_Update_balance_incorrect_after_bet

--- app/Http/Controllers/ProviderService/GameHallController.php
+++ app/Http/Controllers/ProviderService/GameHallController.php
@@ -113,7 +113,7 @@ class GameHallController extends Controller
         $token = $this->betInformation();
         foreach ($token->data->txns as $tokenRaw) {
             $member =  MembersModel::where('id', $tokenRaw->userId)->first();
-            $amountbet = $tokenRaw->betAmount;
+            $amountbet = $tokenRaw->betAmount * $this->ratio;
             $creditMember = $member->credit;
             $amount = $creditMember - $amountbet;
             $this->betTime = $tokenRaw->betTime;
@@ -136,7 +136,7 @@ class GameHallController extends Controller
                 } else {
                     // update credit to table member
                     $member->update([
-                        'credit' => $amount * $this->ratio,
+                        'credit' => $amount,
                         'updated_at' => $tokenRaw->betTime,
                     ]);
                     $bets = BetModel::create([
