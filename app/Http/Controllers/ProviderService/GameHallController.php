72d5319ba152aeadc380a36f882910fd8502b2cd
yoga-saputra1 <86820630+yoga-saputra1@users.noreply.github.com>
2021-12-13 15:24:22 +0700

    Bug/panagdev 1584/panagdev 1638/bug update balance incorrect after bet (#108)

--- app/Http/Controllers/ProviderService/GameHallController.php
+++ app/Http/Controllers/ProviderService/GameHallController.php
@@ -136,7 +136,7 @@ class GameHallController extends Controller
                 } else {
                     // update credit to table member
                     $member->update([
-                        'credit' => $amount * $this->ratio,
+                        'credit' => $amount,
                         'updated_at' => $tokenRaw->betTime,
                     ]);
                     $bets = BetModel::create([
@@ -359,7 +359,7 @@ class GameHallController extends Controller
                 ];
             } else {
 
-                $amountWin = $tokenRaw->winAmount;
+                $amountWin = $tokenRaw->winAmount * $this->ratio;
                 $creditMember = $member->credit;
                 $amount = $creditMember + $amountWin;
                 // update credit to table member
