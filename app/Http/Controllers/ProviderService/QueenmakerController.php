65adc23ffcb21a6a309d0ad36edb73a494a1900e
yoga-saputra1 <86820630+yoga-saputra1@users.noreply.github.com>
2021-12-23 01:51:33 +0700

    Bug/panmdev 1762/panmdev 1774/bug multiple transactions inthe/wallet/credit request (#143)
    * feat/PANMDEV-1498/PANMDEV-1530/3.5._Settle_Bet_Licensee_API_Intergration_To_Rest_API
    
    * BUG/PANMDEV-189/PANMDEV-691/BUG_5.1.3.2.3_Bet_Payout_Player_always_win
    
    * BUG/PANMDEV-189/PANMDEV-691/BUG_5.1.3.2.3_Bet_Payout_Player_always_win
    
    * BUG/PANMDEV-1762/PANMDEV-1774/BUG_Multiple_transactions_inthe/wallet/credit_request

--- app/Http/Controllers/ProviderService/QueenmakerController.php
+++ app/Http/Controllers/ProviderService/QueenmakerController.php
@@ -79,30 +79,8 @@ class QueenmakerController extends Controller
         }else{
             foreach ($token->transactions as $tokenRaw) {
                 // transaction on credit
-                $bet = BetModel::where('bet_id', '=', $tokenRaw->refptxid)->first();
-                
-                $member = MembersModel::find($tokenRaw->userid);
+                $bet = BetModel::where('bet_id', '=', $tokenRaw->ptxid)->first();
                 if ($bet) {
-                    $bet->update([
-                        'bet_id' => $tokenRaw->ptxid,
-                        'refptxid' => $tokenRaw->refptxid,
-                        'win' => $tokenRaw->amt,
-                        'platform' => $tokenRaw->gpcode,
-                        'game_id' => $tokenRaw->gamecode,
-                        'game' => $tokenRaw->gamename,
-                        'game_info' => $tokenRaw->gametype == 0 ? 'slot' : 'TableGame',
-                        'type' => $tokenRaw->txtype === 500 ? 'Bet' : ($tokenRaw->txtype === 510  ? 'Win' : ($tokenRaw->txtype === 511  ? 'Jackpot' : ($tokenRaw->txtype === 520 ? 'Lose' : ($tokenRaw->txtype === 530 ? 'Freebet' : ($tokenRaw->txtype === 540 ? 'Tie' : 'End_round'))))),
-                        'round_id' => $tokenRaw->roundid,
-                        'deskripsi' => $tokenRaw->txtype === 500 ? 'Game Bet' . ' : ' . $tokenRaw->amt : ($tokenRaw->txtype === 510  ? 'Game Win' . ' : ' . $tokenRaw->amt : ($tokenRaw->txtype === 511  ? 'Game Jackpot' . ' : ' . $tokenRaw->amt : ($tokenRaw->txtype === 520 ? 'Game Lose' . ' : ' . $tokenRaw->amt : ($tokenRaw->txtype === 530 ? 'Game Freebet' . ' : ' . $tokenRaw->amt : ($tokenRaw->txtype === 540 ? 'Game Tie' . ' : ' . $tokenRaw->amt : 'End_round'))))),
-                        'created_at' => $tokenRaw->timestamp,
-                        'created_by' => $tokenRaw->userid,
-                        'constant_provider_id' => 9,
-                    ]);
-                    // calculate balance
-                    $balance = $member->credit + $tokenRaw->amt;
-                    $member->update([
-                        'credit' => $balance
-                    ]);
                     return response()->json([ 
                         'transactions' => [
                             ([
@@ -110,7 +88,7 @@ class QueenmakerController extends Controller
                                 'ptxid' => $bet->bet_id,
                                 'bal' => $member->credit,
                                 'cur' => 'IDR',
-                                'dup' => false,
+                                'dup' => true,
                             ])
                         ]
                     ]);
@@ -129,18 +107,26 @@ class QueenmakerController extends Controller
                         'created_at' => $tokenRaw->timestamp,
                         'created_by' => $tokenRaw->userid,
                         'constant_provider_id' => 9,
-                    ]);  
+                    ]);
+
+                    // update credit 
+                    $member = MembersModel::find($tokenRaw->userid);
+                    $balance = $member->credit + $tokenRaw->amt;
+                    $member->update([
+                        'credit' => $balance
+                    ]);
+                    
+                    $transaction = array_push($data, [
+                        "txid"  => $betCreate->id,
+                        "ptxid" => $tokenRaw->ptxid,
+                        "bal"   => $member->credit,
+                        "cur"   => "IDR",
+                        "dup"   => false
+                    ]);
                 }
-                $transaction = array_push($data, [
-                    "txid"  => $betCreate->id,
-                    "ptxid" => $tokenRaw->ptxid,
-                    "bal"   => $member->credit,
-                    "cur"   => "IDR",
-                    "dup"   => true
-                ]);
             }
             return response()->json([
-                'transaction' => $data
+                'transactions' => $data
             ]);
         }
     }
