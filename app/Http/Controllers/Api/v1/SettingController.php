258a6beee050ae5818a52c9fded63fca0d96a1a3
Zikri Cikatech <zikri.cikatech@gmail.com>
2022-02-05 17:03:50 +0700

    Rest API Meta Tag Base on JSON (only Meta Tag)

--- app/Http/Controllers/Api/v1/SettingController.php
+++ app/Http/Controllers/Api/v1/SettingController.php
@@ -58,7 +58,7 @@ class SettingController extends ApiController
 
             return $this->successResponse(null, 'No content', 204);
         } catch (\Throwable $th) {
-            return $this->errorResponse('internal server error', 500);
+            return $this->errorResponse('internal Server Error', 500);
         }
     }
 
