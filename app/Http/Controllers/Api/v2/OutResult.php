34f2678b77d8277923a4777e6f6a4d406930ef3e
CkTch <raymond.ginting@gmail.com>
2022-04-23 20:06:16 +0700

    PANMBDEV-1993/PANAGDEV-966: BUG: Member Backend  TOGEL - market order

--- app/Http/Controllers/Api/v2/OutResult.php
+++ app/Http/Controllers/Api/v2/OutResult.php
@@ -64,7 +64,7 @@ class OutResult extends ApiController
 								'status as is_active'
 							])
 							->with('resultNumber')
-							->orderBy('name', 'asc')
+							->orderBy('id', 'desc')
 							->get();
 		return OutResultResource::collection($results);
 	}
