2898ffd84aff98e36f667bedab93df4f60b4b7fa
yoga-saputra1 <yogasaputra.cikatech@gmail.com>
2022-05-12 20:49:14 +0700

    PANAGDEV877/PANAGDEV1080/BUG: AGENT - Members - WinLoseAll - Detail button at History - Spade Gaming Fish

--- app/Http/Controllers/ProviderService/ProviderController.php
+++ app/Http/Controllers/ProviderService/ProviderController.php
@@ -49,7 +49,7 @@ class ProviderController extends Controller
       $bets = [
         'constant_provider_id' => $data->provider === 'Pragmatic' ? 1 : ($data->provider === 'Habanero' ? 2 : ($data->provider === 'Joker Gaming' ? 3 : ($data->provider === 'Spade Gaming' && $data->type === 'slot' ? 4 : ($data->provider === 'Pg Soft' ? 5 : ($data->provider === 'Playtech' ? 6 : ($data->provider === 'Spade Gaming' && $data->type === 'fish' ? 14 : '')))))),
         'bet_id' => $data->code,
-        'deskripsi' => 'Game '. $status  . ' : ' . $data->amountbet,
+        'deskripsi' => 'Game '. $status  . ' : ' . $amountbet,
         'round_id' => $data->roundId,
         'type' => $status,
         'game_info' => $data->type,
