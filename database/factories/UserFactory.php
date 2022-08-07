566739ba81ade432685f74428648226bb495db98
Zikricikatech <zikri.cikatech@gmail.com>
2022-08-07 16:28:01 +0700

    PANAGDEV-2919/PANAGDEV-2920/DB-Database-Migration-and-Seeder-v.4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,79 +1,33 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\Domains\Auth\Models\User;
-//"WE DON'T NEED THIS FAKER FOR PRODUCTION", DON'T ENABLE THIS
-//use Faker\Generator as Faker;
-//use Illuminate\Support\Str;
-//
-///*
-//|--------------------------------------------------------------------------
-//| Model Factories
-//|--------------------------------------------------------------------------
-//|
-//| This directory should contain each of the model factory definitions for
-//| your application. Factories provide a convenient way to generate new
-//| model instances for testing / seeding your application's database.
-//|
-//*/
-//
-//$factory->define(User::class, function (Faker $faker) {
-//    return [
-//        'type' => $faker->randomElement([User::TYPE_ADMIN, User::TYPE_USER]),
-//        'name' => $faker->name,
-//        'email' => $faker->unique()->safeEmail,
-//        'email_verified_at' => now(),
-//        'password' => 'secret',
-//        'password_changed_at' => null,
-//        'remember_token' => Str::random(10),
-//        'active' => true,
-//    ];
-//});
-//
-//$factory->state(User::class, 'admin', function () {
-//    return [
-//        'type' => User::TYPE_ADMIN,
-//    ];
-//});
-//
-//$factory->state(User::class, 'user', function () {
-//    return [
-//        'type' => User::TYPE_USER,
-//    ];
-//});
-//
-//$factory->state(User::class, 'active', function () {
-//    return [
-//        'active' => true,
-//    ];
-//});
-//
-//$factory->state(User::class, 'inactive', function () {
-//    return [
-//        'active' => false,
-//    ];
-//});
-//
-//$factory->state(User::class, 'confirmed', function () {
-//    return [
-//        'email_verified_at' => now(),
-//    ];
-//});
-//
-//$factory->state(User::class, 'unconfirmed', function () {
-//    return [
-//        'email_verified_at' => null,
-//    ];
-//});
-//
-//$factory->state(User::class, 'password_expired', function () {
-//    return [
-//        'password_changed_at' => now()->subYears(5),
-//    ];
-//});
-//
-//$factory->state(User::class, 'deleted', function () {
-//    return [
-//        'deleted_at' => now(),
-//    ];
-//});
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
