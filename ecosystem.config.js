f0064bfbd6e2756455a4308cc9788967afeb6d1f
Celestine Stephen <decele2011@gmail.com>
2022-11-16 22:37:33 +0100

    fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,28 @@
 module.exports = {
     apps: [
         {
-            name: 'Member-api Websocket',
-            exec_mode: 'cluster',
-            instances: 1, // Or a number of instances
-            script: 'php artisan websockets:serve',
-            //args: 'start',
+            "name": "Member-api Websocket",
+            "script": "artisan",
+            "args": ["websockets:serve"],
+            "instances": "1",
+            "wait_ready": true,
+            "autorestart": true,
+            "max_restarts": 2,
+            "interpreter" : "php",
+            "watch": true,
+            "time": true
         },
         {
-            name: 'Member-api Queue Worker',
-            exec_mode: 'cluster',
-            instances: 1, // Or a number of instances
-            script: 'php artisan queue:work',
-            //args: 'start',
+            "name": "Member-api Queue Worker",
+            "script": "artisan",
+            "args": ["queue:work"],
+            "instances": "1",
+            "wait_ready": true,
+            "autorestart": true,
+            "max_restarts": 2,
+            "interpreter" : "php",
+            "watch": true,
+            "time": true
         },
     ],
 }
