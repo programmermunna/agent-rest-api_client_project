29e9f11bcd5db6c3efbb5c6304b7f357599566e6
Celestine Stephen <decele2011@gmail.com>
2022-11-16 17:18:10 +0100

    fix event none emitting event error

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,7 @@
 Broadcast::channel('App.Models.MembersModel.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('test', function ($user) {
+    return true;
+});
