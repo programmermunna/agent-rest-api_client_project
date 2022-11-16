5e95870084436d48249981353343e78a3afeb274
Zikricikatech <zikri.cikatech@gmail.com>
2022-11-17 02:14:20 +0700

    ft-setup-websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -13,7 +13,7 @@ return [
     |
     | Supported: "pusher", "redis", "log", "null"
     |
-    */
+     */
 
     'default' => env('BROADCAST_DRIVER', 'null'),
 
@@ -26,7 +26,7 @@ return [
     | to broadcast events to other systems or over websockets. Samples of
     | each available type of connection are provided inside this array.
     |
-    */
+     */
 
     'connections' => [
 
@@ -39,9 +39,9 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 //'useTLS' => true,
                 //'encrypted' => true,
-                'host' => env('HOST'),//'127.0.0.1',
+                'host' => env('HOST'), //'127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https',
             ],
         ],
 
