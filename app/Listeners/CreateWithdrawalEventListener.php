177e1a09839f6587d4377803487ebae526d0258d
Gazimehedi <gazimehedihasan512@gmail.com>
2022-12-21 23:14:18 +0600

    PANMBDEV-2766/PANMBDEV-3160-MEMBERAPI-Web-Socket-For-Balance

--- app/Listeners/CreateWithdrawalEventListener.php
+++ app/Listeners/CreateWithdrawalEventListener.php
@@ -33,23 +33,23 @@ class CreateWithdrawalEventListener
         if ($event->getEmitAble()) {
             $withdrawal = $event->getWithdrawModel();
             //sends api call with payload to intending listener
-            // $externalServices = (new OrganizationServiceRepository())->getAllItems();
+            $externalServices = (new OrganizationServiceRepository())->getAllItems();
 
-            // $externalServicesEventDispatcher = new SyncApplicationEventsAmongServices();
-            // foreach ($externalServices as $service) {
-            //     $url = config('app.env') === 'production' ?
-            //         "{$service->production_url}{$service->events_endpoint}" :
-            //         "{$service->staging_url}{$service->events_endpoint}";
+            $externalServicesEventDispatcher = new SyncApplicationEventsAmongServices();
+            foreach ($externalServices as $service) {
+                $url = config('app.env') === 'production' ?
+                    "{$service->production_url}{$service->events_endpoint}" :
+                    "{$service->staging_url}{$service->events_endpoint}";
 
-            //     $externalServicesEventDispatcher->notifyOverREST(
-            //         $url,
-            //         [
-            //             'event' => CreateWithdrawalEvent::class,
-            //             'withdrawal_id' => $withdrawal->id,
-            //             'has_model' => true
-            //         ]
-            //     );
-            // }
+                $externalServicesEventDispatcher->notifyOverREST(
+                    $url,
+                    [
+                        'event' => CreateWithdrawalEvent::class,
+                        'withdrawal_id' => $withdrawal->id,
+                        'has_model' => true
+                    ]
+                );
+            }
             WebhookCall::create()
             ->url(env('WEBHOOK_URL').'/create-withdraw-event')
             ->payload(['withdrawal_id' => $withdrawal->id])
