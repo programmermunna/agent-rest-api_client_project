3ea00c7b61aad3ccf9f79038b0a1f8f79d2200ab
CkTch <raymond.ginting@gmail.com>
2023-04-29 19:50:53 +0700

    test

--- app/Http/Controllers/Api/v1/WithdrawController.php
+++ app/Http/Controllers/Api/v1/WithdrawController.php
@@ -110,7 +110,7 @@ class WithdrawController extends ApiController
                         if ($TOMember < $TO) {
                             return $this->errorResponse('Maaf, Anda belum bisa melakukan withdraw saat ini, karena Anda belum memenuhi persyaratan untuk klaim Bonus New Member. Turnover Anda belum mencapai target saat ini, yaitu sebesar Rp. ' . number_format($TOMember) . '. Turnover yang harus anda capai adalah sebesar Rp. ' . number_format($TO), 400);
                         }
-                        return $this->successResponse(null, '$memberId: '.$memberId);
+                        return $this->successResponse(null, '$bankAsalTransferForWd->id: '.$bankAsalTransferForWd->id);
                         $payload = [
                             'members_id' => $memberId,
                             'rekening_id' => $bankAsalTransferForWd->id,
