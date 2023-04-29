e0620bd34aaaad1ae04cb8564a3a4c27c6d05d2a
Rezi Ferdiansyah <93911253+reziferdiansyah88@users.noreply.github.com>
2023-04-29 19:23:47 +0700

    back-code

--- app/Http/Controllers/Api/v1/WithdrawController.php
+++ app/Http/Controllers/Api/v1/WithdrawController.php
@@ -63,7 +63,6 @@ class WithdrawController extends ApiController
                 ->first();
             if ($bankAsalTransferForWd) {
                 $bonus_freebet = BonusSettingModel::select('status_bonus', 'durasi_bonus_promo', 'min_depo', 'max_depo', 'bonus_amount', 'turnover_x', 'constant_provider_id')->where('constant_bonus_id', 4)->first();
-                // $bonus_freebet = BonusSettingModel::select('status_bonus', 'min_depo', 'max_depo', 'bonus_amount', 'turnover_x', 'constant_provider_id')->where('constant_bonus_id', 4)->first();
                 $bonus_deposit = BonusSettingModel::select('status_bonus', 'durasi_bonus_promo', 'min_depo', 'max_depo', 'max_bonus', 'bonus_amount', 'turnover_x', 'constant_provider_id')->where('constant_bonus_id', 6)->first();
                 # Check Bonus New Member
                 if ($bonus_freebet->status_bonus == 1) {
