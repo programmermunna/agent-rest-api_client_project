701af2e10e10a226e6cdc2a4a80f87b4e3e7dc6a
Rezi Ferdiansyah <93911253+reziferdiansyah88@users.noreply.github.com>
2023-04-30 12:18:48 +0700

    test-is_claim_bonus

--- app/Http/Controllers/Api/v1/DepositController.php
+++ app/Http/Controllers/Api/v1/DepositController.php
@@ -99,7 +99,7 @@ class DepositController extends ApiController
                 $today = Carbon::now()->format('Y-m-d');
                 $check_claim_bonus = DepositModel::where('members_id', $this->memberActive->id)
                     ->where('approval_status', 1)
-                    ->whereIn('is_claim_bonus', [4, 6])
+                    ->whereIn('is_claim_bonus', 6)
                     ->whereDate('approval_status_at', $today)->orderBy('approval_status_at', 'desc')->first();
                     // TESTING DD 
                      DB::commit();
