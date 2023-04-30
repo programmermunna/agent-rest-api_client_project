286fd54e44346c774b517061ba0c2b111c9354e9
Rezi Ferdiansyah <93911253+reziferdiansyah88@users.noreply.github.com>
2023-04-30 12:31:15 +0700

    test-is_claim_bonus

--- app/Http/Controllers/Api/v1/DepositController.php
+++ app/Http/Controllers/Api/v1/DepositController.php
@@ -102,8 +102,8 @@ class DepositController extends ApiController
                     ->whereIn('is_claim_bonus', [4, 6])
                     ->whereDate('approval_status_at', $today)->orderBy('approval_status_at', 'desc')->first();
                     // TESTING DD 
-                     DB::commit();
-                     return $this->successResponse(null, 'DD TEST =>' .$check_claim_bonus->is_claim_bonus);
+                    //  DB::commit();
+                    //  return $this->successResponse(null, 'DD TEST =>' .$check_claim_bonus->is_claim_bonus);
                 if ($bonus_deposit->status_bonus == 1) {
                     if (count($check_claim_bonus) > $bonus_deposit->limit_claim) {
                         if ($check_claim_bonus->is_claim_bonus == 6) {
