9b3f78379e46311cf97a9178fb1a8cbf2eea69e3
Rezi Ferdiansyah <93911253+reziferdiansyah88@users.noreply.github.com>
2023-04-30 12:41:51 +0700

    test-is_claim_bonus

--- app/Http/Controllers/Api/v1/DepositController.php
+++ app/Http/Controllers/Api/v1/DepositController.php
@@ -106,10 +106,11 @@ class DepositController extends ApiController
                     //  TESTING 
                     ->whereIn('is_claim_bonus', [4, 6])
                     ->whereDate('approval_status_at', $today)->orderBy('approval_status_at', 'desc')->first();
-                    //  DB::commit();
-                    //  return $this->successResponse(null, 'DD TEST =>' .$check_claim_bonus->is_claim_bonus);
+                   
+                     DB::commit();
+                     return $this->successResponse(null, 'DD TEST =>' .$check_claim_bonus->is_claim_bonus);
                 if ($bonus_deposit->status_bonus == 1) {
-                    if (count($check_claim_bonus->is_claim_bonus) > $bonus_deposit->limit_claim) {
+                    if (count($check_claim_bonus) > $bonus_deposit->limit_claim) {
                         if ($check_claim_bonus->is_claim_bonus == 6) {
                             return $this->errorResponse("Maaf, Bonus Existing Member dapat diklaim sehari maksimal {$bonus_deposit->limit_claim} kali.", 400);
                         }                        
