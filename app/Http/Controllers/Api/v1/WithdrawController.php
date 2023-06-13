7bd39c733bafb431d79b6362741c8c69630ee146
Zikricikatech <zikri.cikatech@gmail.com>
2023-06-13 19:46:45 +0700

    Fixing Withdraw

--- app/Http/Controllers/Api/v1/WithdrawController.php
+++ app/Http/Controllers/Api/v1/WithdrawController.php
@@ -330,7 +330,7 @@ class WithdrawController extends ApiController
             if ($bonus_existing->status_bonus == 1) {
                 $checkBonusExisting = TurnoverMember::where('member_id', $memberId)->where('constant_bonus_id', 6)
                     ->whereBetween('created_at', [$subDay, $today])
-                // ->where('status', true)
+                    ->where('status', '!=', 2)
                     ->whereNull('withdraw_id')->get();
                 if ($checkBonusExisting->toArray() != []) {
                     foreach ($checkBonusExisting as $key => $existingMemberBonus) {
