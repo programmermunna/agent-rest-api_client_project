0c55094afd7d0812347eb038658dba83c93dbbf6
Zikricikatech <zikri.cikatech@gmail.com>
2023-07-29 16:54:05 +0700

    2023 agenda juli new agent beruang988

--- config/cikatechMaster.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'url_check_maintenance_agent' => env('URL_CHECK_MAINTENANCE'),
-    'agent_ip' => env('AGENT_IP'),
-    'agent_name' => env('AGENT_NAME'),
-    'secret_url' => env('SECRET_URL'),
-];
