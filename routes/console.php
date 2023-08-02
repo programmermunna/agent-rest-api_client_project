08aaf446a163fd5bfda073c5361cd185f8d66e98
Zikricikatech <zikri.cikatech@gmail.com>
2023-08-02 09:46:49 +0700

    2023.agenda.august.togel.multiple.submit.result

--- routes/console.php
+++ routes/console.php
@@ -11,8 +11,18 @@ use Illuminate\Foundation\Inspiring;
 | commands. Each Closure is bound to a command instance allowing a
 | simple approach to interacting with each command's IO methods.
 |
-*/
+ */
 
 //Artisan::command('inspire', function () {
 //    $this->comment(Inspiring::quote());
 //})->describe('Display an inspiring quote');
+
+Artisan::command('migrate', function () {
+    $this->comment("Sorry, You are not allowed to do this!\nBy : Cikatech\nTime : " . now());
+})->describe('Override default command.');
+Artisan::command('migrate:fresh', function () {
+    $this->comment("Sorry, You are not allowed to do this!\nBy : Cikatech\nTime : " . now());
+})->describe('Override default command.');
+Artisan::command('migrate:refresh', function () {
+    $this->comment("Sorry, You are not allowed to do this!\nBy : Cikatech\nTime : " . now());
+})->describe('Override default command.');
