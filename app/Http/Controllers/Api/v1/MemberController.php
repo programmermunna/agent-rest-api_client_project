5da218418be8b4190db3b57440991e5cb54cc242
Zikricikatech <zikri.cikatech@gmail.com>
2023-08-03 18:22:42 +0700

    2023.agenda.august.togel.history.transaction

--- app/Http/Controllers/Api/v1/MemberController.php
+++ app/Http/Controllers/Api/v1/MemberController.php
@@ -2625,7 +2625,7 @@ class MemberController extends ApiController
             ->selectRaw("
             max(bets_togel.id) as id,
             bets_togel.created_by,
-            min(bets_togel.balance) as balance,
+            IF(SUM(bets_togel.win_lose_status) > 0, MAX(bets_togel.balance), MIN(bets_togel.balance)) as balance,
             constant_provider_togel.id as constant_provider_togel_id,
             bets_togel.created_at
 
